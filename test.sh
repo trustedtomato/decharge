dad6ee4b03d67d67bc527e96cb95a0b41e2b8686
Tamas Halasi <trusted.tomato@gmail.com>
2021-07-28 23:16:02 +0200

    chore: extract testing to a file
    This way the developer can use the testing script, so they can be sure about the success of the test

---
+++ test.sh
@@ -0,0 +1,23 @@
+#/usr/bin/env bash
+
+# This test script is used for the test GH Action.
+# Useful for fully checking if your code is working.
+
+pnpm install
+
+cd packages/protester
+pnpm run build
+cd ../..
+
+cd packages/decharge
+pnpm run build
+pnpm run test
+cd ../..
+
+cd examples/project-website
+pnpm run build
+cd ../..
+
+cd examples/blog
+pnpm run build
+cd ../..
